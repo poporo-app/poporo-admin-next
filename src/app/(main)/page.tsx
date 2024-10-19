5cdb0cb6cf4c13aa77cdf33a888521f8df01c13a
junichi nasu <j-nasu@po2ro.com>
2024-10-19 22:11:03 +0900

    デーバック

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,4 +1,18 @@
+import { useEffect } from 'react'
+
 const Dashbord = () => {
+  useEffect(() => {
+    console.log('Application started')
+    console.log('Environment:', process.env.NODE_ENV)
+    console.log(
+      'NEXT_PUBLIC_FIREBASE_API_KEY:',
+      process.env.NEXT_PUBLIC_FIREBASE_API_KEY
+        ? process.env.NEXT_PUBLIC_FIREBASE_API_KEY.substr(0, 5) + '...'
+        : 'Not set'
+    )
+    console.log('NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN:', process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN)
+    console.log('NEXT_PUBLIC_FIREBASE_PROJECT_ID:', process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID)
+  }, [])
   return <div>Dashbord</div>
 }
 
